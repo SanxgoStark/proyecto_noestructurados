70e4159f3f8204700cd27ee1b361c36e73ad6cc6
SanxgoStark <17030203@itcelaya.edu.mx>
2022-11-10 18:15:31 -0600

    cambios de prueba

--- proyecto_noestructurados/Form1.cs
+++ proyecto_noestructurados/Form1.cs
@@ -16,5 +16,10 @@ namespace proyecto_noestructurados
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
